f8427d447487ac45e687b1f4d1f6310c2d3e912c
jessecardenas <jessecardenas@users.noreply.github.com>
2025-01-12 03:32:54 -0600

    click to hide

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+console.log("test");
+
+// Click an item to remove
+let myNodes;
+myNodes = document.getElementById("content_list").children;
+var i;
+for (i = 0; i < myNodes.length; i++) {
+  myNodes[i].onclick = function() { this.style.display = "none"; }
+}
+
+
+
